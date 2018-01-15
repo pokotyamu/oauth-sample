d554b683d699c720f16eec3c16dbace3b0264fce
pokotyamu <pokotyamu@gmail.com>
2018-01-15 18:29:00 +0900

    http リクエストレベルでの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  def create
+    client = HTTPClient.new
+
+    parameters = {
+      client_id: ENV['COLORMESHOP_CLIENT_ID'],
+      client_secret: ENV['COLORMESHOP_CLIENT_SECRET'],
+      code: params[:code],
+      grant_type: 'authorization_code',
+      redirect_uri: 'http://localhost:3000/auth/colorme/callback'
+    }
+
+    res = client.post('https://api.shop-pro.jp/oauth/token', parameters)
+
+    @account = Account.create(name: Time.now, access_token: JSON.parse(res.body)['access_token'])
+    render :create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/index'
 
+  get '/auth/:provider/callback' => 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
